cf83541e4a359372b39ed1320f213faa231122c0
xuhaojie <779868946@qq.com>
2020-01-29 21:50:24 +0800

    111

---
+++ src/main/java/com/xhj/bloom/CountingBloomFilterTest.java
@@ -0,0 +1,23 @@
+package com.xhj.bloom;
+
+import orestes.bloomfilter.CountingBloomFilter;
+import orestes.bloomfilter.FilterBuilder;
+
+/**
+ * https://github.com/Baqend/Orestes-Bloomfilter
+ */
+public class CountingBloomFilterTest {
+    public static void main(String[] args) {
+        CountingBloomFilter<String> cbf = new FilterBuilder(1000,
+                0.01).buildCountingBloomFilter();
+        cbf.add("http://gupaoedu.com");
+        cbf.add("http://alibaba.com");
+        cbf.add("http://baidu.com");
+
+        cbf.remove("http://baidu.com");
+
+        System.out.println(cbf.contains("http://gupaoedu.com")); //true
+        System.out.println(cbf.contains("http://alibaba.com")); //true
+        System.out.println(cbf.contains("http://baidu.com")); //false
+    }
+}
