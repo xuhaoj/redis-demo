c6c4abd29a8550ca8cd310e8f36f2267ca4f34c7
xuhaojie <779868946@qq.com>
2020-01-29 21:31:31 +0800

    111

---
+++ src/main/java/com/xhj/redisdemo/bloom/RedisMain.java
@@ -0,0 +1,79 @@
+package com.xhj.redisdemo.bloom;
+
+import com.google.common.hash.Funnels;
+import com.google.common.hash.Hashing;
+import redis.clients.jedis.Jedis;
+
+import java.nio.charset.Charset;
+
+public class RedisMain {
+    static final int expectedInsertions = 100;//要插入多少数据
+    static final double fpp = 0.01;//期望的误判率
+
+    //bit数组长度
+    private static long numBits;
+
+    //hash函数数量
+    private static int numHashFunctions;
+
+    static {
+        numBits = optimalNumOfBits(expectedInsertions, fpp);
+        numHashFunctions = optimalNumOfHashFunctions(expectedInsertions, numBits);
+    }
+
+    public static void main(String[] args) {
+        Jedis jedis = new Jedis("192.168.0.109", 6379);
+        for (int i = 0; i < 100; i++) {
+            long[] indexs = getIndexs(String.valueOf(i));
+            for (long index : indexs) {
+                jedis.setbit("codebear:bloom", index, true);
+            }
+        }
+        for (int i = 0; i < 100; i++) {
+            long[] indexs = getIndexs(String.valueOf(i));
+            for (long index : indexs) {
+                Boolean isContain = jedis.getbit("codebear:bloom", index);
+                if (!isContain) {
+                    System.out.println(i + "肯定没有重复");
+                }
+            }
+            System.out.println(i + "可能重复");
+        }
+    }
+
+    /**
+     * 根据key获取bitmap下标
+     */
+    private static long[] getIndexs(String key) {
+        long hash1 = hash(key);
+        long hash2 = hash1 >>> 16;
+        long[] result = new long[numHashFunctions];
+        for (int i = 0; i < numHashFunctions; i++) {
+            long combinedHash = hash1 + i * hash2;
+            if (combinedHash < 0) {
+                combinedHash = ~combinedHash;
+            }
+            result[i] = combinedHash % numBits;
+        }
+        return result;
+    }
+
+    private static long hash(String key) {
+        Charset charset = Charset.forName("UTF-8");
+        return Hashing.murmur3_128().hashObject(key, Funnels.stringFunnel(charset)).asLong();
+    }
+
+    //计算hash函数个数
+    private static int optimalNumOfHashFunctions(long n, long m) {
+        return Math.max(1, (int) Math.round((double) m / n * Math.log(2)));
+    }
+
+    //计算bit数组长度
+    private static long optimalNumOfBits(long n, double p) {
+        if (p == 0) {
+            p = Double.MIN_VALUE;
+        }
+        return (long) (-n * Math.log(p) / (Math.log(2) * Math.log(2)));
+    }
+}
+
--- src/test/java/com/xhj/redisdemo/RedisDemoApplicationTests.java
+++ src/test/java/com/xhj/redisdemo/RedisDemoApplicationTests.java
@@ -1,13 +1,21 @@
 package com.xhj.redisdemo;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class RedisDemoApplicationTests {
 
+/*    @Autowired
+    RedisUtil util;*/
+
     @Test
     void contextLoads() {
+/*        util.set("boot", "2673--" + System.currentTimeMillis());
+        System.out.println(util.get("boot"));*/
     }
 
 }
